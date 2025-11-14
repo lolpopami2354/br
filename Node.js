657fca42b73465d0bda34bcff9e6b5954915024f
lolpopami2354 <cyborgv43214@gmail.com>
2025-11-13 17:04:05 -0800

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,16 @@
+{
+  "name": "simple-web-proxy",
+  "version": "1.0.0",
+  "description": "Minimal HTTP/HTTPS proxy with tunneling and CORS",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "cors": "2.8.5",
+    "express": "4.19.2",
+    "http-proxy": "1.18.1",
+    "morgan": "1.10.0",
+    "body-parser": "1.20.2"
+  }
+}
